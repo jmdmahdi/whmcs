3fea1c93ec81e3bf17f8592338bf2ca188096cbd
Ali Jazayeri - Ubuntu <jaza.ali@gmail.com>
2018-11-12 15:47:40 +0330

    first commit

---
+++ modules/gateways/callback/idpay.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * IDPay payment gateway
+ *
+ * @developer JMDMahdi
+ * @publisher IDPay
+ * @copyright (C) 2018 IDPay
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ *
+ * http://idpay.ir
+ */
+
+require_once __DIR__ . '/../../../init.php';
+$whmcs->load_function('gateway');
+$whmcs->load_function('invoice');
+
+if (!defined("WHMCS")) die();
+
+$gatewayModuleName = basename(__FILE__, '.php');
+
+$gatewayParams = getGatewayVariables($gatewaymodule);
+
+if (!$gatewayParams['type']) die('Module Not Activated');
+
+function idpay_get_failed_message($failed_massage, $track_id, $order_id)
+{
+    return str_replace(["{track_id}", "{order_id}"], [$track_id, $order_id], $failed_massage);
+}
+
+function idpay_get_success_message($success_massage, $track_id, $order_id)
+{
+    return str_replace(["{track_id}", "{order_id}"], [$track_id, $order_id], $success_massage);
+}
+
+function idpay_end()
+{
+    global $orderid, $CONFIG, $paymentSuccess;
+    if (isset($orderid) && $orderid) {
+        callback3DSecureRedirect($orderid, $paymentSuccess);
+        exit();
+    } else {
+        header('Location: ' . $CONFIG['SystemURL'] . '/clientarea.php?action=invoices');
+        exit();
+    }
+}
+
+$paymentSuccess = false;
+$orderid = $_POST['order_id'];
+$amount = $_POST['amount'];
+$orderid = checkCbInvoiceID($orderid, $gatewayParams['name']);
+
+$pid = $_POST['id'];
+$porder_id = $_POST['order_id'];
+
+if (!empty($pid) && !empty($porder_id) && $porder_id == $orderid) {
+    $api_key = $gatewayParams['api_key'];
+    $sandbox = $gatewayParams['sandbox'] == 'on' ? 'true' : 'false';
+
+    $data = array(
+        'id' => $pid,
+        'order_id' => $orderid,
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1/payment/inquiry');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'X-API-KEY:' . $api_key,
+        'X-SANDBOX:' . $sandbox,
+    ));
+
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($http_status != 200) {
+        logTransaction($gatewayParams['name'],
+            [
+                "GET" => $_GET,
+                "POST" => $_POST,
+                "result" => sprintf('خطا هنگام بررسی وضعیت تراکنش. کد خطا: %s', $http_status)
+            ], 'Failure');
+        idpay_end();
+    }
+
+    $inquiry_status = empty($result->status) ? NULL : $result->status;
+    $inquiry_track_id = empty($result->track_id) ? NULL : $result->track_id;
+    $inquiry_amount = empty($result->amount) ? NULL : $result->amount;
+
+    checkCbTransID($inquiry_track_id);
+
+    if (empty($inquiry_status) || empty($inquiry_track_id) || empty($inquiry_amount) || $inquiry_amount != $amount || $inquiry_status != 100) {
+        logTransaction($gatewayParams['name'],
+            [
+                "GET" => $_GET,
+                "POST" => $_POST,
+                "result" => idpay_get_failed_message($gatewayParams['failed_massage'], $inquiry_track_id, $orderid)
+            ], 'Failure');
+    } else {
+        $paymentSuccess = true;
+        addInvoicePayment($orderid, $inquiry_track_id, $amount, 0, $gatewaymodule);
+        logTransaction($gatewayParams['name'],
+            [
+                "GET" => $_GET,
+                "POST" => $_POST,
+                "result" => idpay_get_success_message($gatewayParams['success_massage'], $inquiry_track_id, $orderid)
+            ], 'Success');
+    }
+} else {
+    logTransaction($gatewayParams['name'],
+        [
+            "GET" => $_GET,
+            "POST" => $_POST,
+            "result" => 'کاربر از انجام تراکنش منصرف شده است'
+        ], 'Failure');
+}
+
+idpay_end();
---
+++ modules/gateways/idpay.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * IDPay payment gateway
+ *
+ * @developer JMDMahdi
+ * @publisher IDPay
+ * @copyright (C) 2018 IDPay
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ *
+ * http://idpay.ir
+ */
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+function idpay_config()
+{
+    return [
+        "FriendlyName" => [
+            "Type" => "System",
+            "Value" => "IDPay"
+        ],
+        "Currencies" => [
+            "FriendlyName" => "واحد پولی",
+            "Type" => "dropdown",
+            "Options" => "Rial,Toman"
+        ],
+        "api_key" => [
+            "FriendlyName" => "API KEY",
+            "Type" => "text"
+        ],
+        "sandbox" => [
+            "FriendlyName" => "آزمایشگاه",
+            "Type" => "yesno"
+        ],
+        "success_massage" => [
+            "FriendlyName" => "پیام پرداخت موفق",
+            "Type" => "textarea",
+            "Value" => "پرداخت شما با موفقیت انجام شد. کد رهگیری: {track_id}",
+            "Description" => "متن پیامی که می خواهید بعد از پرداخت موفق به کاربر نمایش دهید را وارد کنید. همچنین می توانید از شورت کدهای {order_id} برای نمایش شماره سفارش و {track_id} برای نمایش کد رهگیری آیدی پی استفاده نمایید."
+        ],
+        "failed_massage" => [
+            "FriendlyName" => "پیام پرداخت ناموفق",
+            "Type" => "textarea",
+            "Value" => "پرداخت شما ناموفق بوده است. لطفا مجددا تلاش نمایید یا در صورت بروز اشکال با مدیر سایت تماس بگیرید.",
+            "Description" => "متن پیامی که می خواهید بعد از پرداخت ناموفق به کاربر نمایش دهید را وارد کنید. همچنین می توانید از شورت کدهای {order_id} برای نمایش شماره سفارش و {track_id} برای نمایش کد رهگیری آیدی پی استفاده نمایید."
+        ]
+    ];
+}
+
+function idpay_link($params)
+{
+    $systemurl = $params['systemurl'];
+    $api_key = $params['api_key'];
+    $sandbox = $params['sandbox'] == 'on' ? 'true' : 'false';
+    $amount = intval($params['amount']);
+    $moduleName = $params['paymentmethod'];
+    if (!empty($params['Currencies']) && $params['Currencies'] == "Toman") {
+        $amount *= 10;
+    }
+
+    $desc = $params["description"];
+    $callback = $systemurl . '/modules/gateways/callback/' . $moduleName . '.php';
+
+    if (empty($amount)) {
+        return 'واحد پول انتخاب شده پشتیبانی نمی شود.';
+    }
+
+    $data = array(
+        'order_id' => $params['invoiceid'],
+        'amount' => $amount,
+        'phone' => $params['clientdetails']['phonenumber'],
+        'desc' => $desc,
+        'callback' => $callback,
+    );
+
+    $ch = curl_init('https://api.idpay.ir/v1/payment');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'X-API-KEY:' . $api_key,
+        'X-SANDBOX:' . $sandbox,
+    ));
+
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($http_status != 201 || empty($result) || empty($result->id) || empty($result->link)) {
+        return sprintf('خطا هنگام ایجاد تراکنش. کد خطا: %s', $http_status);
+    } else {
+        return '<form method="get" action="' . $result->link . '"><input type="submit" name="pay" value="پرداخت" /></form>';
+    }
+}
