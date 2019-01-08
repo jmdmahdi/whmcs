21ecce2354f285bf43c33a50f456e04247bc8102
jmdmahdi <jmdmahdi@users.noreply.github.com>
2019-01-08 21:24:59 +0330

    fix a bug

--- modules/gateways/callback/idpay.php
+++ modules/gateways/callback/idpay.php
@@ -43,80 +43,82 @@ function idpay_end()
 }
 
 $paymentSuccess = false;
-$orderid = $_POST['order_id'];
-$amount = $_POST['amount'];
-$orderid = checkCbInvoiceID($orderid, $gatewayParams['name']);
+$orderid = 0;
+if(!empty($_POST['order_id'])){
+	$orderid = $_POST['order_id'];
+	$amount = $_POST['amount'];
+	$orderid = checkCbInvoiceID($orderid, $gatewayParams['name']);
 
-$pid = $_POST['id'];
-$porder_id = $_POST['order_id'];
+	$pid = $_POST['id'];
+	$porder_id = $_POST['order_id'];
 
-if (!empty($pid) && !empty($porder_id) && $porder_id == $orderid) {
-    $api_key = $gatewayParams['api_key'];
-    $sandbox = $gatewayParams['sandbox'] == 'on' ? 'true' : 'false';
+	if (!empty($pid) && !empty($porder_id) && $porder_id == $orderid) {
+		$api_key = $gatewayParams['api_key'];
+		$sandbox = $gatewayParams['sandbox'] == 'on' ? 'true' : 'false';
 
-    $data = array(
-        'id' => $pid,
-        'order_id' => $orderid,
-    );
+		$data = array(
+			'id' => $pid,
+			'order_id' => $orderid,
+		);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1/payment/inquiry');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json',
-        'X-API-KEY:' . $api_key,
-        'X-SANDBOX:' . $sandbox,
-    ));
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1/payment/inquiry');
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json',
+			'X-API-KEY:' . $api_key,
+			'X-SANDBOX:' . $sandbox,
+		));
 
-    $result = curl_exec($ch);
-    $result = json_decode($result);
-    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
+		$result = curl_exec($ch);
+		$result = json_decode($result);
+		$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
 
-    if ($http_status != 200) {
-        logTransaction($gatewayParams['name'],
-            [
-                "GET" => $_GET,
-                "POST" => $_POST,
-                "result" => sprintf('خطا هنگام بررسی وضعیت تراکنش. وضعیت خطا: %s - کد خطا: %s - پیام خطا: %s', $http_status, $result->error_code, $result->error_message)
-            ], 'Failure');
-        idpay_end();
-    }
+		if ($http_status != 200) {
+			logTransaction($gatewayParams['name'],
+				[
+					"GET" => $_GET,
+					"POST" => $_POST,
+					"result" => sprintf('خطا هنگام بررسی وضعیت تراکنش. وضعیت خطا: %s - کد خطا: %s - پیام خطا: %s', $http_status, $result->error_code, $result->error_message)
+				], 'Failure');
+			idpay_end();
+		}
 
-    $inquiry_status = empty($result->status) ? NULL : $result->status;
-    $inquiry_track_id = empty($result->track_id) ? NULL : $result->track_id;
-    $inquiry_amount = empty($result->amount) ? NULL : $result->amount;
+		$inquiry_status = empty($result->status) ? NULL : $result->status;
+		$inquiry_track_id = empty($result->track_id) ? NULL : $result->track_id;
+		$inquiry_amount = empty($result->amount) ? NULL : $result->amount;
 
-    checkCbTransID($inquiry_track_id);
+		checkCbTransID($inquiry_track_id);
 
-    if (empty($inquiry_status) || empty($inquiry_track_id) || empty($inquiry_amount) || $inquiry_amount != $amount || $inquiry_status != 100) {
-        logTransaction($gatewayParams['name'],
-            [
-                "GET" => $_GET,
-                "POST" => $_POST,
-                "result" => idpay_get_failed_message($gatewayParams['failed_massage'], $inquiry_track_id, $orderid)
-            ], 'Failure');
-    } else {
-        $paymentSuccess = true;
-        if (!empty($gatewayParams['Currencies']) && $gatewayParams['Currencies'] == 'Toman') {
-          $amount = $amount / 10;
-        }
-        addInvoicePayment($orderid, $inquiry_track_id, $amount, 0, $gatewaymodule);
-        logTransaction($gatewayParams['name'],
-            [
-                "GET" => $_GET,
-                "POST" => $_POST,
-                "result" => idpay_get_success_message($gatewayParams['success_massage'], $inquiry_track_id, $orderid)
-            ], 'Success');
-    }
-} else {
-    logTransaction($gatewayParams['name'],
-        [
-            "GET" => $_GET,
-            "POST" => $_POST,
-            "result" => 'کاربر از انجام تراکنش منصرف شده است'
-        ], 'Failure');
+		if (empty($inquiry_status) || empty($inquiry_track_id) || empty($inquiry_amount) || $inquiry_amount != $amount || $inquiry_status != 100) {
+			logTransaction($gatewayParams['name'],
+				[
+					"GET" => $_GET,
+					"POST" => $_POST,
+					"result" => idpay_get_failed_message($gatewayParams['failed_massage'], $inquiry_track_id, $orderid)
+				], 'Failure');
+		} else {
+			$paymentSuccess = true;
+			if (!empty($gatewayParams['Currencies']) && $gatewayParams['Currencies'] == 'Toman') {
+			  $amount = $amount / 10;
+			}
+			addInvoicePayment($orderid, $inquiry_track_id, $amount, 0, $gatewaymodule);
+			logTransaction($gatewayParams['name'],
+				[
+					"GET" => $_GET,
+					"POST" => $_POST,
+					"result" => idpay_get_success_message($gatewayParams['success_massage'], $inquiry_track_id, $orderid)
+				], 'Success');
+		}
+	} else {
+		logTransaction($gatewayParams['name'],
+			[
+				"GET" => $_GET,
+				"POST" => $_POST,
+				"result" => 'کاربر از انجام تراکنش منصرف شده است'
+			], 'Failure');
+	}
 }
-
 idpay_end();
